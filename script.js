1398970233e766a9c95b4780cdb4ed31c52fbe9d
LucaBert89 <62511269+LucaBert89@users.noreply.github.com>
2020-08-18 20:52:55 +0200

    modified

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-let api1 = process.env.API_KEY;
-let api2 = process.env.API_KEY2;
 
+const cityBtn = document.getElementById("addcityBtn");
+const suggestBtn = document.querySelector("#suggestBtn");
+const closeBtn = document.querySelector("#closeBtn");
 
-
-let cityBtn = document.getElementById("addcityBtn");
-let suggestBtn = document.querySelector("#suggestBtn");
-
-
+let locations = document.querySelector("#locations");
+let locationsWeather = document.querySelector("#locationsWeather");
 let iconWeather = document.querySelector("#weatherIcon");
 let cityName = document.querySelector("#cityToday");
 let temperature = document.querySelector("#temp")
@@ -14,10 +12,10 @@ let minTemperature = document.querySelector("#min");
 let maxTemperature = document.querySelector("#max");
 let searchCity = document.querySelector("#searchCity");
 let lakes = [];
-let message = document.querySelector("#errorMessage");
+let message = document.querySelectorAll(".errorMessage");
 
-let latitude;
-let longitude;
+let cityLatitude;
+let cityLongitude;
 let arraylakesLat = [];
 let arraylakesLong = [];
 
@@ -34,8 +32,19 @@ let arraylakesLong = [];
             cityLongitude = position.coords.longitude;
             map.setCenter({lat:cityLatitude, lng:cityLongitude});
             map.setZoom(10);
-        });
 
+            let url = `https://api.openweathermap.org/data/2.5/weather?lat=${cityLatitude}&lon=${cityLongitude}&units=metric&appid=${process.env.API_KEY2}&lang=US`;
+            fetch(url)  
+                .then(res => {
+                return res.json();
+            })
+            .then(body => {
+                weatherData(body);
+                displayWeather();
+            })
+        })
+    
+    closeBtn.addEventListener("click", closeForecast)
     cityBtn.addEventListener("click", cityWeather);
     suggestBtn.addEventListener("click", suggestLocation);
 })();
@@ -68,52 +77,46 @@ var defaultLayers = platform.createDefaultLayers();
 
 
 /*API calls for city temperature and weather */
-function cityWeather() {
-    let city = searchCity.value;
-    let apiKey = process.env.API_KEY2;
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.API_KEY2}&lang=US`;
-    
-    fetch(url)  
-        .then(res => {
-        return res.json();
-    })
-    .then(body => {
-        message.innerHTML = "";
-        cityLatitude = body.coord.lat;
-        cityLongitude = body.coord.lon;
-        icon = body.weather[0].icon;
-        nameC = body.name;
-        temp = body.main.temp;
-        tempMax = body.main.temp_max;
-        tempMin = body.main.temp_min;   
-    })
-    .then (function () {
-        displayWeather();
-        map.setCenter({lat:cityLatitude, lng:cityLongitude});
-        map.setZoom(10);
-    })
-    .catch(error => {
-        message.innerHTML = "Nome della località non trovato!"
-    })}
-
-    function displayWeather () {
-        iconWeather.setAttribute("src", "https://openweathermap.org/img/w/" + icon + ".png" ); 
-        cityName.innerHTML = nameC  ;
-        temperature.innerHTML = `${temp} °C`;
-        minTemperature.innerHTML = `${tempMin} °C`;
-        maxTemperature.innerHTML = `${tempMax} °C`;
-        return;
+    function cityWeather() {
+        let city = searchCity.value;
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.API_KEY2}&lang=US`;
+        
+        fetch(url)  
+            .then(res => {
+            return res.json();
+        })
+        .then(body => {
+            weatherData(body);
+        })
+        .then (function () {
+            displayWeather();
+            map.setCenter({lat:cityLatitude, lng:cityLongitude});
+            map.setZoom(10);
+        })
+        .catch(error => {
+            return message[0].innerHTML = "Nome della località non trovato!"
+        })
     }
+
+        function displayWeather () {
+            iconWeather.setAttribute("src", `https://openweathermap.org/img/w/` + icon + `.png` ); 
+            temperature.innerHTML = `${temp} °C`;
+            minTemperature.innerHTML = `${tempMin} °C`;
+            maxTemperature.innerHTML = `${tempMax} °C`;
+            return;
+        }
 
 
 /*button to retrieve lakes location and then call marker function */
     function suggestLocation() {
-        
+        message[1].innerHTML = ""
+        if(cityLatitude === undefined) {
+            return message[1].innerHTML = "You need to insert a location or allow geolocation!"
+        }
         map.setCenter({lat:cityLatitude, lng:cityLongitude});
         map.setZoom(10);
 
         limits = 20;
-        let apiKey = process.env.API_KEY;
 
         let url= `https://browse.search.hereapi.com/v1/browse?at=${cityLatitude},${cityLongitude}&limit=${limits}&categories=350-3500-0304&apikey=${process.env.API_KEY}`
         
@@ -136,7 +139,7 @@ function cityWeather() {
         }
         
 /*build the markers on the map adding a click event to retrieve latitude and longitude*/
-function addMarkersToMap(map) {
+    function addMarkersToMap(map) {
    
         removeMarker(lakes);
         for(i=0; i < limits; i++) {
@@ -149,22 +152,34 @@ function addMarkersToMap(map) {
         for(i=0; i < limits;i++) {
         lakes[i].addEventListener('tap', function (evt) {
             var lakePos = map.screenToGeo(evt.currentPointer.viewportX,evt.currentPointer.viewportY);
+            var x = window.matchMedia("(max-width: 760px)")
+            if (x.matches) { 
+                weatherOverlay();
+            }
             apiWeather(lakePos.lat, lakePos.lng);
             apiName(lakePos.lat, lakePos.lng);
+            
         });
         }
 
         function removeMarker(marker) {
             if(map.getObjects(marker).length == limits){
                 map.removeObjects(marker);
-            }}
+            }}    
+    }
             
-}
-            
+    function weatherOverlay() {
+        if(locations.getAttribute("class") == "container pb-5 active") {
+            locations.classList.remove("active");
+            locationsWeather.remove("overlay");
+        } else {
+            locations.classList.add("active");
+            locationsWeather.classList.add("overlay");
+        }
+    }
 
 /*pass longitude and latitude to weather API */
     function apiWeather(lat, lon) {
-          let apiKey = process.env.API_KEY2;
           let url =  `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.API_KEY2}&lang=US`;
           fetch(url)  
           .then(res => {
@@ -186,25 +201,38 @@ function addMarkersToMap(map) {
                 dayWeather[daytag].innerHTML = dayName;
                 icon = body.daily[day].weather[0].icon;
                 forecast[daytag].innerHTML = body.daily[day].weather[0].description;
-                iconWeather[daytag].setAttribute("src", "http://openweathermap.org/img/w/" + icon + ".png" );
+                iconWeather[daytag].setAttribute("src", "https://openweathermap.org/img/w/" + icon + ".png" );
                 temp[daytag].innerHTML = body.daily[day].temp.day + "°C";
             }
             dayForecast(0,1)
             dayForecast(1,2)
             dayForecast(2,3)
         }
-        )}
+        )
+    }
 
     function apiName(lat, lon) {
-        let apiKey = process.env.API_KEY;
         let url = `https://browse.search.hereapi.com/v1/browse?at=${lat},${lon}&limit=${limits}&categories=350-3500-0304&apikey=${process.env.API_KEY}`;
         fetch(url)  
             .then(res => {
             return res.json();
         })
         .then(body => {
-            console.log(body);
             let lakeForecast = document.querySelector("#lakeForecast");
             lakeForecast.innerHTML = body.items[0].title;
-    })}
+        })
+    }
 
+    function weatherData(bdy) {
+        message[0].innerHTML = "";
+        cityLatitude = bdy.coord.lat;
+        cityLongitude = bdy.coord.lon;
+        icon = bdy.weather[0].icon;
+        temp = bdy.main.temp;
+        tempMax = bdy.main.temp_max;
+        tempMin = bdy.main.temp_min; 
+    }
+
+    function closeForecast (){
+        locations.classList.remove("active");
+    }
