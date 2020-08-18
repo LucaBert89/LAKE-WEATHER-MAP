9bdb5608320b67b28bb01cf879e59b7f9a3d101d
LucaBert89 <62511269+LucaBert89@users.noreply.github.com>
2020-08-18 11:33:50 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,14 @@
 const path = require("path");
 const Dotenv = require('dotenv-webpack');
-const HtmlWebpackPlugin = require(`html-webpack-plugin`);
+
 module.exports = {
     entry:"./script.js",
     mode:"production",
     output:{
         filename:"main.js",
-        path :path.resolve(__dirname,"build")
+        path :path.resolve(__dirname,"dist")
     },
     plugins: [
-        new Dotenv(),
-        new HtmlWebpackPlugin({
-            template: path.resolve(`./index.html`)
-        })
+        new Dotenv()
       ]
   };
